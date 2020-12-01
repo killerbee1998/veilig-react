72a53e2e381b98222da210a38bca35ef4d0865fb
riasat1998 <riasat1998@gmail.com>
2020-12-01 19:07:25 +0600

    components/body/Login: created with header

---
+++ src/components/body/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () =>{
+    return(
+        
+        <section class="coloured-section" id="login">
+            <div class = "container-header">
+                <h1 id="header"> Veilig </h1>
+            </div>
+
+        </section>
+    )
+}
