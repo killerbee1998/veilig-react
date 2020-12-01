c55d8662b55df661a2699236627c56ceb993aad3
riasat1998 <riasat1998@gmail.com>
2020-12-01 20:23:11 +0600

    created Register component

---
+++ src/components/body/Register/Regsiter.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import './Register.css'
+
+const Register = () =>{
+    return(
+        
+        <section className="coloured-section" id="login">
+            <div className = "container-header">
+                <h1 id="header"> Veilig </h1>
+            </div>
+            <div className="container-form">
+
+                <h1>Login</h1>
+
+                <form>
+                    <div className = 'form-group'>
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                    </div>
+
+                    <div type="submit" class="btn btn-primary btn-lg" id="btn-submit-login">Submit</div>
+
+                </form>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Register
