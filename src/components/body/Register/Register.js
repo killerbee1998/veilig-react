3a89820634871ce3644edb1dc99977a21b4c69e9
riasat1998 <riasat1998@gmail.com>
2020-12-02 18:57:17 +0600

    added states to register component

--- src/components/body/Register/Register.js
+++ src/components/body/Register/Register.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './Register.css'
 
 const Register = () =>{
+
+    const [registerEmail, setRegisterEmail] = useState('')
+    const [registerPass, setRegisterPass] = useState('')
+
     return(
         
         <section className="coloured-section" id="register">
