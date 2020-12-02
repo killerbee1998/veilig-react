d878231dbbb5237852a669dabb76cfaddb1d511a
riasat1998 <riasat1998@gmail.com>
2020-12-02 18:56:02 +0600

    added a new url for register

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,7 @@
 const ROOT_URL = 'https://mighty-anchorage-78746.herokuapp.com/'
 
 const loginUrl = ROOT_URL+'account/login'
+const registerUrl = ROOT_URL+'account/register'
 
 module.exports.loginUrl = loginUrl
+module.exports.registerUrl = registerUrl 
