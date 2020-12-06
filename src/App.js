4cb2460966100291a96cb4718d96c18f5aa8d4b1
riasat1998 <riasat1998@gmail.com>
2020-12-06 19:34:58 +0600

    created a new navbar component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import Login from './components/body/Login/Login'
 import Register from './components/body/Register/Register'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <Register></Register>
+      <Navbar/>
+      <Register/>
     </div>
   );
 }
