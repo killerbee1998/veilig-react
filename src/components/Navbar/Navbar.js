222657ce3081a967bf8115369e4deb2dab8c5823
riasat1998 <riasat1998@gmail.com>
2020-12-09 19:45:33 +0600

    created state management to display navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import './Navbar.css'
 
-const Navbar = () =>{
-    const Tabs = ['Home', '']
+const Navbar = ({loginStatus}) =>{
+    const [navBody, setNavBody] = useState(<div className = 'navItem'> Log Out</div>)
+    useEffect( ()=>{
+        switch(loginStatus){
+            case 'LoggedIn':
+                setNavBody(<div className = 'navItem'> Log Out</div>)
+                break
+            case 'onRegister':
+                setNavBody(<div className = 'navItem'> Log In</div>)
+                break                
+            default:
+                setNavBody(<div className = 'navBody'>
+                    <div className = 'navItem'> Log In</div>
+                    <div className = 'navItem'> Register</div>
+                    </div>)
+                break
+
+        }
+    })
     return(
         <nav>
-            <div id = 'logout'> Log Out</div>
+            {navBody}
         </nav>
     )
 }
