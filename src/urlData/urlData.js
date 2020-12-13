c6ab5f9d74f07ff259ef0450f5787a95aacc9037
riasat1998 <riasat1998@gmail.com>
2020-12-13 23:11:10 +0600

    created a new displayPassUrl

--- src/urlData/urlData.js
+++ src/urlData/urlData.js
@@ -3,5 +3,7 @@ const ROOT_URL = 'https://mighty-anchorage-78746.herokuapp.com/'
 const loginUrl = ROOT_URL+'account/login'
 const registerUrl = ROOT_URL+'account/register'
 
+const displayPassUrl = ROOT_URL+'store/displayPass'
+
 module.exports.loginUrl = loginUrl
 module.exports.registerUrl = registerUrl 
