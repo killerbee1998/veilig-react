4ee197b4930cb98a390e34ca216104b7d4f88ddc
riasat1998 <riasat1998@gmail.com>
2020-12-16 20:08:32 +0600

    added a new savePassUrl

--- src/urlData/urlData.js
+++ src/urlData/urlData.js
@@ -4,6 +4,7 @@ const loginUrl = ROOT_URL+'account/login'
 const registerUrl = ROOT_URL+'account/register'
 
 const displayPassUrl = ROOT_URL+'store/displayPass'
+const savePassUrl = ROOT_URL+'store/savePass'
 
 const genPassUrl = ROOT_URL+'pass'
 const genPasshraseUrl = ROOT_URL+'passphrase'
@@ -12,6 +13,7 @@ module.exports.loginUrl = loginUrl
 module.exports.registerUrl = registerUrl 
 
 module.exports.displayPassUrl = displayPassUrl
+module.exports.savePassUrl = savePassUrl
 
 module.exports.genPassUrl = genPassUrl
 module.exports.genPassphraseUrl = genPasshraseUrl
