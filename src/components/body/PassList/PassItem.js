0229a8387b71969d47a07eafbf7142833fe6ff19
riasat1998 <riasat1998@gmail.com>
2020-12-16 20:55:55 +0600

    added custom css to PassItem

--- src/components/body/PassList/PassItem.js
+++ src/components/body/PassList/PassItem.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap'
+import {Card} from 'react-bootstrap'
+
+import './PassItem.css'
 
 const PassItem = () =>{
     return(
-        <Card style={{ width: '18rem' }}>
+        <Card class='cardHolder'>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>User Password Item</Card.Title>
+                <Card.Text> User Name</Card.Text>
+                <Card.Text> Url</Card.Text>
+                <Card.Text> Password</Card.Text>
             </Card.Body>
         </Card>
     )
